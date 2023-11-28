c57c3bfc9fcbe23a6dbdcea878a90140d181e7d9
Tobias Senti <git@tsenti.li>
2023-11-28 15:37:17 +0000

    Generate Tech Lef

--- config/generate.py
+++ config/generate.py
@@ -4,8 +4,11 @@ import json
 from mako.template import Template
 
 config_file_name = "./config/pdk.json"
+lef_file_name = "./config/lef.json"
 liberty_prefix = "liberty74_"
+lef_name = "liberty74"
 lib_path = "./lib/"
+lef_path = "./lef/"
 
 # Load JSON file
 config_file = open(config_file_name)
@@ -43,18 +46,58 @@ for c in corners:
     
     print("Done!")
 
-# Load Footprint JSON
-#footprint_file = open(footprint_file_name)
-#footprint_json = json.load(footprint_file)
+# Load LEF JSON
+lef_file = open(lef_file_name)
+lef_json = json.load(lef_file)
 
 # Read Footprints
-#footprints = {}
-#for footprint_data in footprint_json["footprints"]:
-    #for name in footprint_data["names"]:
-        #footprints[name] = footprint_data
+footprints = {}
+for footprint_data in lef_json["footprints"]:
+    for name in footprint_data["names"]:
+        footprints[name] = footprint_data
 
-#footprint_file.close()
+# Technology Info
+technology = lef_json["technology"]
 
-# Generate LEF
-#for cell in cells:
+lef_file.close()
+
+# Generate Footprints
+for fp in footprints:
+    footprints[fp]["cell_width"] = 3.0
+    footprints[fp]["cell_height"] = technology["row_height"]
+
+# Load Tech LEF template
+tech_lef_template = Template(filename="./config/tech_lef.template")
+
+tech_lef_context = {
+    "technology": technology
+}
+
+print("Generating Tech LEF...")
+
+rendered_tech_lef = tech_lef_template.render(**tech_lef_context)
+
+with open(lef_path + lef_name + "_tech.lef", 'w', encoding='utf-8') as tech_lef_file:
+    tech_lef_file.write(rendered_tech_lef)
     
+print("Done!")
+
+# Load LEF template
+lef_template = Template(filename="./config/lef.template")
+
+lef_context = {
+    "footprints": footprints,
+    "cells": cells,
+    "site_width": technology["site_width"],
+    "row_height": technology["row_height"]
+}
+
+print("Generating LEF...")
+
+rendered_lef = lef_template.render(**lef_context)
+
+with open(lef_path + lef_name + ".lef", 'w', encoding='utf-8') as lef_file:
+    lef_file.write(rendered_lef)
+    
+print("Done!")
+
