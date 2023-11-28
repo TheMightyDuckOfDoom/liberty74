22d811f6ceab85665d535df33d0233df4cc56d5d
Tobias Senti <git@tsenti.li>
2023-11-28 15:37:18 +0000

    Generate Vias at Ring and Followpin Interception

--- utils/def2pcb.py
+++ utils/def2pcb.py
@@ -92,27 +92,117 @@ pcb.traceItems.append(outline)
 print('Adding special nets...')
 pcb.nets = []
 for idx, net in enumerate(def_parser.specialnets):
-    pcb.nets.append(KiNet(number = idx, name = net.name))
+    pcb.nets.append(KiNet(number = idx + 1, name = net.name))
+
+def trimSegment(segment: BrdSegment):
+    if segment.start.X == segment.end.X:
+        if segment.start.Y > segment.end.Y:
+            segment.start.Y -= segment.width / 2
+            segment.end.Y   += segment.width / 2
+        if segment.start.Y < segment.end.Y:
+            segment.start.Y += segment.width / 2
+            segment.end.Y   -= segment.width / 2
+    if segment.start.Y == segment.end.Y:
+        if segment.start.X > segment.end.X:
+            segment.start.X -= segment.width / 2
+            segment.end.X   += segment.width / 2
+        if segment.start.X < segment.end.X:
+            segment.start.X += segment.width / 2
+            segment.end.X   -= segment.width / 2
+    return segment
+
+def find_intercept(start1: KiPosition, end1: KiPosition, start2: KiPosition, end2: KiPosition):
+    # Check if orthogonal to each other
+    if start1.X == end1.X and start2.X == end2.X:
+        return None
+    if start1.Y == end1.Y and start2.Y == end2.Y:
+        return None
+
+    # One is horizontal and the other one is vertical
+    if start2.X == end2.X:
+        # Make start1-end1 vrtical
+        temp = start1
+        start1 = start2
+        start2 = temp
+
+        temp = end1
+        end1 = end2
+        end2 = temp
+
+    # start1-end1 is vertical
+    # start2-end2 is horizontal
+
+    # Make start always smaller
+    if start1.Y > end1.Y:
+        temp = start1.Y 
+        start1.Y = end1.Y
+        end1.Y = temp
+
+    if start2.X > end2.X:
+        temp = start2.X 
+        start2.X = end2.X
+        end2.X = temp
+
+    # Check if Y of horizontal line is within bounds of vertical line
+    if start2.Y < start1.Y or start2.Y > end1.Y:
+        return None
+
+    # Check if X of vertical line is within bounds of horizontal line
+    if start1.X < start2.X or start1.X > end2.X:
+        return None
+    
+    # They have to intercept at Y of horizontal line and X of vertical line
+
+    return KiPosition(X = start1.X, Y = start2.Y)
 
 # Add special nets routing
 print('Generating power grid...')
-for net_idx, net in enumerate(def_parser.specialnets):
+
+for net in def_parser.specialnets:
     print(net.name)
 
+    ring_shapes = list(filter(lambda x: x.shape_type == 'RING' and x.end_via == None, net.shapes))
+
     for shape in net.shapes:
-        print(shape.shape_type)
+        net_idx = next((x.number for x in pcb.nets if x.name == net.name), 0)
         
         if shape.end_via == None:
+            start = KiPosition(shape.points[0][0] / scale, shape.points[0][1] / scale)
+            end   = KiPosition(shape.points[1][0] / scale, shape.points[1][1] / scale)
+
+            # Generate Via at end of segment
+            if shape.shape_type == 'FOLLOWPIN':
+                # Find intercept with rings
+                for ring in ring_shapes:
+                    interception = find_intercept(start, end, KiPosition(ring.points[0][0] / scale, ring.points[0][1] / scale), KiPosition(ring.points[1][0] / scale, ring.points[1][1] / scale))
+
+                    if interception == None:
+                        continue
+                    
+                    via = BrdVia(
+                        type = None,
+                        locked = False,
+                        position = interception,
+                        size = via_size,
+                        drill = via_drill_size,
+                        layers = metal_layers,
+                        free = False,
+                        net = net_idx,
+                        tstamp = 'via_powergrid_interception_' + shape.layer
+                    )
+                    pcb.traceItems.append(via)
+                
             # Segment
             segment = BrdSegment(
-                start = KiPosition(shape.points[0][0] / scale, shape.points[0][1] / scale),
-                end = KiPosition(shape.points[1][0] / scale, shape.points[1][1] / scale),
+                start = start,
+                end = end,
                 width = float(shape.width) / scale,
                 layer = shape.layer,
                 locked = False,
                 net = net_idx,
                 tstamp = 'seg_powergrid_' + shape.layer + '_' + str(shape.points[0][0]) + '_' + str(shape.points[0][1]) + '_' + str(shape.points[1][0]) + '_' + str(shape.points[1][1])
             )
+            segment = trimSegment(segment)
             pcb.traceItems.append(segment)
         else:
             # Via
@@ -125,14 +215,14 @@ for net_idx, net in enumerate(def_parser.specialnets):
                 layers = metal_layers,
                 free = False,
                 net = net_idx,
-                tstamp = 'via_powergrid_' + shape.layer + '_' + str(shape.end_via_loc[0]) + '_' + str(shape.end_via_loc[1])
+                tstamp = 'via_powergrid_' + shape.shape_type.lower() + '_' + shape.layer + '_' + str(shape.end_via_loc[0]) + '_' + str(shape.end_via_loc[1])
             )
             pcb.traceItems.append(via)
 
 
 # Add nets
 print('Adding nets...')
-offset = len(pcb.nets)
+offset = pcb.nets[-1].number + 1
 for idx, net in enumerate(def_parser.nets):
     pcb.nets.append(KiNet(number = idx + offset, name = net.name))
 
@@ -140,7 +230,9 @@ for idx, net in enumerate(def_parser.nets):
 # Add net routing
 
 print("Generating net routing...")
-for net_idx, net in enumerate(def_parser.nets):
+
+
+for net in def_parser.nets:
     print(net.name)
     
     for route in net.routed:
@@ -152,7 +244,7 @@ for net_idx, net in enumerate(def_parser.nets):
                 width = wire_width,
                 layer = route.layer,
                 locked = False,
-                net = net_idx,
+                net = next((x.number for x in pcb.nets if x.name == net.name), 0),
                 tstamp = 'seg_' + route.layer + '_' + str(route.points[0][0]) + '_' + str(route.points[0][1]) + '_' + str(route.points[1][0]) + '_' + str(route.points[1][1])
             )
             pcb.traceItems.append(segment)
@@ -166,7 +258,7 @@ for net_idx, net in enumerate(def_parser.nets):
                 drill = via_drill_size,
                 layers = metal_layers,
                 free = False,
-                net = net_idx,
+                net = next((x.number for x in pcb.nets if x.name == net.name), 0),
                 tstamp = 'via_' + route.layer + '_' + str(route.end_via_loc[0]) + '_' + str(route.end_via_loc[1])
             )
             pcb.traceItems.append(via)
